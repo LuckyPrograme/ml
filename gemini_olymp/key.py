0ae6393fb23e9db61ca8da1afe61a0a58978527f
LuckyPrograme <157396472+LuckyPrograme@users.noreply.github.com>
2025-12-17 15:12:35 +0300

    Update key.py

--- gemini_olymp/key.py
+++ gemini_olymp/key.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 
 # ХРАНИТСЯ В РЕПОЗИТОРИИ
-KEY_PREFIX = "AIzaSyDXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # всё, кроме 5 последних
+KEY_PREFIX = "AIzaSyBhsae0N8Z09lTvXDbDbZ48gnpqbrb"  # всё, кроме 4 последних
 
 KEY_FILE = Path.home() / ".gemini_last5"
 
 
 def get_api_key():
     if KEY_FILE.exists():
-        last5 = KEY_FILE.read_text().strip()
+        last4 = KEY_FILE.read_text().strip()
     else:
-        last5 = input("Enter last 5 symbols of Gemini API key: ").strip()
-        if len(last5) != 5:
-            raise ValueError("Need exactly 5 symbols")
-        KEY_FILE.write_text(last5)
+        last4 = input("Enter last 4 symbols of Gemini API key: ").strip()
+        if len(last4) != 4:
+            raise ValueError("Need exactly 4 symbols")
+        KEY_FILE.write_text(last4)
 
-    return KEY_PREFIX + last5
+    return KEY_PREFIX + last4
